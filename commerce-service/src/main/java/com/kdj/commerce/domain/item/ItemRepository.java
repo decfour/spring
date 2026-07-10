ed4350e1b1c610f3e863bf6b41cbf1f9f5fc87c1
decfour <kbig1204@gmail.com>
2026-07-10 21:38:50 +0900

    260710 README 업데이트

--- commerce-service/src/main/java/com/kdj/commerce/domain/item/ItemRepository.java
+++ commerce-service/src/main/java/com/kdj/commerce/domain/item/ItemRepository.java
@@ -1,11 +1,13 @@
 package com.kdj.commerce.domain.item;
 
 import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -16,6 +18,7 @@ import java.util.Optional;
 public interface ItemRepository extends JpaRepository<Item, Long> {
     // 동시성 제어 (비관적 Lock)
     @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints({@QueryHint(name = "javax.persistence.lock.timeout", value = "3000")})
     @Query("select i from Item i where i.id = :id")
     Optional<Item> findByIdWithLock(@Param("id") Long id);
 
